1b1eb7cfb0f80472f21b5ffa4df2ce4dd979c3ce
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-05-19 19:40:44 -0500

    Agregar archivos de proyecto.

---
+++ RepasosBD4/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace RepasosBD4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.nuevoRegistroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.retirarRegistroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.entreRangoDeFechasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.entreRangoDeHorasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.entreRangoDeFechasYHorasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.mantenimientoToolStripMenuItem,
+            this.reportesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // mantenimientoToolStripMenuItem
+            // 
+            this.mantenimientoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultaToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.nuevoRegistroToolStripMenuItem,
+            this.retirarRegistroToolStripMenuItem});
+            this.mantenimientoToolStripMenuItem.Name = "mantenimientoToolStripMenuItem";
+            this.mantenimientoToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.mantenimientoToolStripMenuItem.Text = "Mantenimiento";
+            // 
+            // consultaToolStripMenuItem
+            // 
+            this.consultaToolStripMenuItem.Name = "consultaToolStripMenuItem";
+            this.consultaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.consultaToolStripMenuItem.Text = "Consulta";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // nuevoRegistroToolStripMenuItem
+            // 
+            this.nuevoRegistroToolStripMenuItem.Name = "nuevoRegistroToolStripMenuItem";
+            this.nuevoRegistroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nuevoRegistroToolStripMenuItem.Text = "Nuevo Registro";
+            // 
+            // retirarRegistroToolStripMenuItem
+            // 
+            this.retirarRegistroToolStripMenuItem.Name = "retirarRegistroToolStripMenuItem";
+            this.retirarRegistroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.retirarRegistroToolStripMenuItem.Text = "Retirar Registro";
+            // 
+            // reportesToolStripMenuItem
+            // 
+            this.reportesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.entreRangoDeFechasToolStripMenuItem,
+            this.entreRangoDeHorasToolStripMenuItem,
+            this.entreRangoDeFechasYHorasToolStripMenuItem});
+            this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
+            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.reportesToolStripMenuItem.Text = "Reportes";
+            // 
+            // entreRangoDeFechasToolStripMenuItem
+            // 
+            this.entreRangoDeFechasToolStripMenuItem.Name = "entreRangoDeFechasToolStripMenuItem";
+            this.entreRangoDeFechasToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.entreRangoDeFechasToolStripMenuItem.Text = "Entre rango de fechas";
+            // 
+            // entreRangoDeHorasToolStripMenuItem
+            // 
+            this.entreRangoDeHorasToolStripMenuItem.Name = "entreRangoDeHorasToolStripMenuItem";
+            this.entreRangoDeHorasToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.entreRangoDeHorasToolStripMenuItem.Text = "Entre rango de horas";
+            // 
+            // entreRangoDeFechasYHorasToolStripMenuItem
+            // 
+            this.entreRangoDeFechasYHorasToolStripMenuItem.Name = "entreRangoDeFechasYHorasToolStripMenuItem";
+            this.entreRangoDeFechasYHorasToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.entreRangoDeFechasYHorasToolStripMenuItem.Text = "Entre rango de fechas y horas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::RepasosBD4.Properties.Resources.head_bg27;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Horas y Fechas";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem nuevoRegistroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem retirarRegistroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem entreRangoDeFechasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem entreRangoDeHorasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem entreRangoDeFechasYHorasToolStripMenuItem;
+    }
+}
+
